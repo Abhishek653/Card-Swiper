2a97fc3987e48fd6588134db8cbcd53c54c9e636
Abhishek <abhishekmaurya653@gmail.com>
2024-03-13 00:53:50 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let swiper = new Swiper('.Slider-container', {
+  direction: 'horizontal',
+  effect: 'cards',
+  grabCursor: true,
+  centerdSlides: true,
+  loop: true,
+});
+
+
+// swiper.changeDirection('vertical');
